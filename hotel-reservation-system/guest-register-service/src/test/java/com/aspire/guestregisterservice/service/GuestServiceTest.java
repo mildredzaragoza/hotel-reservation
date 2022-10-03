c8242f0265ebb324cc31753bcec9023107158e0b
mildred.zaragoza <mildred.zaragoza@aspiresys.com>
2022-10-03 17:55:52 -0500

    Annotations were added to test classes.

--- hotel-reservation-system/guest-register-service/src/test/java/com/aspire/guestregisterservice/service/GuestServiceTest.java
+++ hotel-reservation-system/guest-register-service/src/test/java/com/aspire/guestregisterservice/service/GuestServiceTest.java
@@ -3,6 +3,7 @@ package com.aspire.guestregisterservice.service;
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,11 +11,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.aspire.guestregisterservice.models.Guest;
 
 @SpringBootTest
+@DisplayName("Test CRUD operations")
 class GuestServiceTest {
 	@Autowired
 	GuestService guestService;
 	
 	@Test
+	@DisplayName("Test save guest")
 	public void saveGuestTest() throws Exception {
 		Guest guestOne = new Guest();
 		guestOne.setName("Test 1");
@@ -26,30 +29,31 @@ class GuestServiceTest {
 		guestService.saveGuest(guestOne);
 		Assertions.assertTrue(guestOne.getIdGuest() != null);
 		
-		//Missing email
+		//Missing guest's data
 		Guest guest = new Guest();
 		guest.setName("Test 2");
-		guest.setPhoneNumber("1523689715");
-		guest.setTypeGuest("plus");
-		guest.setCheckInDate("2022-08-09");
-		guest.setCheckOutDate("2022-08-09");
-		guestService.saveGuest(guest);
-		Assertions.assertTrue(guest.getIdGuest() == null);
+		Assertions.assertThrows(Exception.class, () -> guestService.saveGuest(guest));
 	}
 	
 	@Test
+	@DisplayName("Test display all guests")
 	public void getAllGuestTest() throws Exception {
 		ArrayList<Guest> guests = guestService.getAllGuests();
 		Assertions.assertTrue(guests.size() > 0);
 	}
 	
 	@Test
+	@DisplayName("Test get guest by id")
 	public void getGuestTest() throws Exception {
 		Guest guest = guestService.getGuestById(30L).get();
 		Assertions.assertTrue(guest != null);
+		
+		//Guest with id 80 does not exists
+		Assertions.assertThrows(Exception.class, () -> guestService.getGuestById(80L).get());
 	}
 	
 	@Test
+	@DisplayName("Test update guest")
 	public void updateGuestTest() throws Exception {
 		Guest guest = new Guest();
 		guest.setName("Test One");
@@ -58,10 +62,18 @@ class GuestServiceTest {
 		guest.setTypeGuest("plus");
 		guest.setCheckInDate("2022-08-09");
 		guest.setCheckOutDate("2022-08-09");
-		guestService.updateGuest(40L, guest);
+		Assertions.assertNotNull(guestService.updateGuest(30L, guest));
+		Assertions.assertThrows(Exception.class, () -> guestService.updateGuest(80L, guest)); 
+		
+		//Missing guest's data
+		Guest guestTwo = new Guest();
+		guestTwo.setName("Test Two");
+		Assertions.assertThrows(Exception.class, () -> guestService.updateGuest(30L, guestTwo)); 
+		
 	}
 	
 	@Test 
+	@DisplayName("Test delete guest")
 	public void testDeleteGuest() throws Exception {
 		guestService.deleteGuest(39L);
 	}
