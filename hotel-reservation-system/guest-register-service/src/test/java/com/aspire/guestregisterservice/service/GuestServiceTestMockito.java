93135e5f1b5f8f5acd63d40c92cb9e65d891f7e5
mildred.zaragoza <mildred.zaragoza@aspiresys.com>
2022-10-31 18:23:21 -0600

    Testing service class in booking service

--- hotel-reservation-system/guest-register-service/src/test/java/com/aspire/guestregisterservice/service/GuestServiceTestMockito.java
+++ hotel-reservation-system/guest-register-service/src/test/java/com/aspire/guestregisterservice/service/GuestServiceTestMockito.java
@@ -1,9 +1,32 @@
 package com.aspire.guestregisterservice.service;
 
-import javax.inject.Inject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.aspire.guestregisterservice.repository.GuestRepository;
+
+@ExtendWith(MockitoExtension.class)
 public class GuestServiceTestMockito {
+	
+	@Mock
+	private GuestRepository guestRepository;
 
-    @Inject
+    @Mock
     private GuestService guestService;
+    
+	@BeforeEach
+    public void init() {
+    	MockitoAnnotations.openMocks(this);
+    }
+    
+    @Test
+    public void getAllGuestTest() throws Exception {
+    	Assertions.assertNotNull(guestService.getAllGuests());
+    }
+
 }
