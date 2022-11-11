e98454d80bc9358131060afd0e6a31430e84f01a
mildred.zaragoza <mildred.zaragoza@aspiresys.com>
2022-11-11 14:41:51 -0600

    Testing CrudController in the main application

--- hotel-reservation-system/web-app/src/main/java/com/aspire/webapp/controller/CrudController.java
+++ hotel-reservation-system/web-app/src/main/java/com/aspire/webapp/controller/CrudController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +23,7 @@ public class CrudController {
 	@Autowired
     BookService bookService;
 	
-    @RequestMapping("/guests")
+    @GetMapping("/guests")
     public String getGuests(Model model){
     	model.addAttribute("guestList", bookService.getGuests().getBody());
         return "show-all-guest";
--- hotel-reservation-system/web-app/src/test/java/com/aspire/webapp/controller/CrudControllerUnitTest.java
+++ hotel-reservation-system/web-app/src/test/java/com/aspire/webapp/controller/CrudControllerUnitTest.java
@@ -1,19 +1,21 @@
 package com.aspire.webapp.controller;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import javax.sql.DataSource;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -21,8 +23,9 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.aspire.webapp.model.Guest;
 import com.aspire.webapp.service.BookService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-@RunWith(SpringRunner.class)
 @WebMvcTest(CrudController.class)
 class CrudControllerUnitTest {
 
@@ -32,21 +35,60 @@ class CrudControllerUnitTest {
 	@MockBean
 	private BookService bookService;
 	
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+    private ObjectMapper objectMapper;
+	
+	@MockBean
+	private DataSource dataSource;
 	
 	@Test
 	@DisplayName("Get all guests test")
 	public void getGuestTest() throws Exception {
-	//	ResponseEntity<Guest> guestOne = new ResponseEntity<Guest>(new Guest(), HttpStatus.OK);
-		ResponseEntity<Guest[]> guests = null;
+		Guest[] guestArray = {new Guest(), new Guest()};
+		ResponseEntity<Guest[]> guests = new ResponseEntity<Guest[]>(guestArray, HttpStatus.OK);
 		when(bookService.getGuests()).thenReturn(guests);
 		mockMvc.perform(MockMvcRequestBuilders.get("/guests"))
-      //  .andExpect(MockMvcResultMatchers.status().isOk())
-        .andExpect(MockMvcResultMatchers.model().attribute("guestList", guests))
-        .andExpect(MockMvcResultMatchers.view().name("show-all-guest"))
-        .andDo(MockMvcResultHandlers.print());
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.model().attribute("guestList", guests.getBody()))
+        .andExpect(MockMvcResultMatchers.view().name("show-all-guest"));
+	}
+	
+	@Test
+	@DisplayName("Add guest test")
+	public void addGuestTest() throws Exception {
+		ResponseEntity<Guest> newGuest = new ResponseEntity<Guest>(new Guest(), HttpStatus.OK);
+		when(bookService.addGuest(any(Guest.class))).thenReturn(newGuest);
+		mockMvc.perform(MockMvcRequestBuilders.post("/add-guest")
+				.content(objectMapper.writeValueAsString(new Guest()))
+    			.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.forwardedUrl("/guests"))
+				.andDo(MockMvcResultHandlers.print());
+	}
+	
+	@Test
+	@DisplayName("Delete guest test")
+	public void deleteGuestTest() throws Exception{
+		ResponseEntity<Boolean> deleteResponse = new ResponseEntity<Boolean>(true, HttpStatus.OK);
+		when(bookService.deleteGuest(5L)).thenReturn(deleteResponse);
+		mockMvc.perform(MockMvcRequestBuilders.delete("/delete-guest-{id}", 5L)
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.forwardedUrl("/guests"))
+				.andDo(MockMvcResultHandlers.print());
+	}
+	
+	@Test
+	@DisplayName("Edit guest's information test")
+	public void editGuestInformationTest() throws Exception {
+		ResponseEntity<Guest> guestToEdit = new ResponseEntity<Guest>(new Guest(), HttpStatus.OK);
+		when(bookService.getGuestById(5L)).thenReturn(guestToEdit);
+		mockMvc.perform(MockMvcRequestBuilders.get("/edit-guest-{id}", 5L)
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.model().attribute("guest", guestToEdit.getBody()))
+				.andExpect(MockMvcResultMatchers.view().name("/guest-form"))
+				.andDo(MockMvcResultHandlers.print());
 	}
 	
 
